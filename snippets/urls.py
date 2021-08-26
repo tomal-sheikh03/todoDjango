21af1ca842934a2bd481f293724897286e81d9e3
Md. Wahidul Islam <69080614+tomal-sheikh03@users.noreply.github.com>
2021-08-26 22:41:46 +0600

    Update urls.py

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('todo/list/', views.snippet_list),
+    path('todo/create/', views.snippet_create),
+    path('todo/<int:pk>/', views.snippet_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
